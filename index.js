c8f38c06f5a8c0aa55d495b9ba93b2d0ea48d4bf
Alberto Leal <mail4alberto@gmail.com>
2014-01-12 06:08:54 -0500

    add domain for catching errors in streaming mode.
    Very rough code atm.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var
 spawn = require('child_process').spawn,
 path = require("path"),
+fs = require('fs'),
+domain = require('domain'),
 
+tmp = require('tmp'),
 _queue = require('async-queue-stream'),
 gutil = require('gulp-util');
 
 
 function gulp_spawn_shim(_opts) {
 
-    if(!_opts.cmd)
+    var opts = _opts || {};
+
+    if(!opts.cmd)
         throw new Error('Command for child_process.spawn is required');
 
-    if(typeof _opts.cmd !== 'string')
+    if(typeof opts.cmd !== 'string')
         throw new Error('Command for child_process.spawn must be a string');
 
-    var opts = _opts || {};
 
     // Spawn args as defined in http://nodejs.org/api/child_process.html
     opts.cmd = _opts.cmd;
@@ -44,12 +48,30 @@ function gulp_spawn_shim(_opts) {
 
     var _write = function(file, cb) {
 
-        config_args(file);
-
-        var child = spawn(opts.cmd, opts.args, opts.options);
-
         // pass along
         if (file.isNull()) return cb(null, file);
+
+        config_args(file);
+
+        // create domain
+        var err_catcher = domain.create();
+
+        err_catcher.on('error', function(err) {
+            console.log('CAUGHT ERROR: ' + err);
+            return cb(err);
+        });
+
+
+        var child = void 0;
+
+        child = spawn(opts.cmd, opts.args, opts.options);
+
+        // catch a lot of errors
+        err_catcher.add(child);
+        err_catcher.add(child.stdin);
+        err_catcher.add(child.stdout);
+        err_catcher.add(child.stderr);
+        err_catcher.add(file.contents);
 
         // error handling
         var err = '';
@@ -58,28 +80,81 @@ function gulp_spawn_shim(_opts) {
             err += data;
         });
 
-        var publish = function(file) {
+        var publish = function(f) {
             if(err.length <= 0)
                 err = null;
-
-            return cb(err, file);
+            return cb(err, f);
         };
+
 
         if (file.isStream()) {
 
             var es = require('event-stream');
 
-            file.contents = es.pipeline(file.contents,
-                                        child.stdin,
-                                        child.stdout);
+            // really fucking lame
+            var stdout = es.pipeline(file.contents,
+                                    child.stdin,
+                                    child.stdout);
 
-            // Emit error
-            child.stderr.once('end', function () {
-                if(err)
-                    file.contents.emit('error', new Error(err));
+            err_catcher.add(stdout);
+            // .on('error', function() {console.log("q")});
+
+            // file.contents
+            // .on('error', function() {console.log("p")});
+
+            // child.stdin
+            // .on('error', function() {console.log("p")});
+
+            // child.stdout
+            // .on('error', function() {console.log("p")});
+
+            // stdout.once('data', function() {
+
+
+
+
+            // });
+            // console.log(data)
+            tmp.file(function _tempFileCreated(err, tmp_path, fd) {
+                if (err) throw err;
+
+                // console.log(Object.keys());
+                if(child.stdout.readable === false) {
+                    tmp.setGracefulCleanup();
+                    // console.log("HERE");
+                    return;
+                }
+
+                // read from tmp_file and write into it
+                var tmp_file = fs.createWriteStream(tmp_path);
+
+                stdout
+                    .on('error', function() {
+                        console.log('bullshit');
+                    })
+                    .on('end', function(){
+
+                        file.contents = fs.createReadStream(tmp_path);
+
+                        console.log('published: '+ file.path);
+                        return publish(file);
+                    });
+
+                stdout
+                    .pipe(tmp_file);
+
+
+
+              // console.log("File: ", path);
+              // console.log("Filedescriptor: ", fd);
+              // tmp.setGracefulCleanup();
             });
 
-            return publish(file);
+
+
+            return;
+
+            // return publish(file);
             // return this.emit('error', new gutil.PluginError('gulp-pandoc', 'Streaming not supported.'));
 
         } else if (file.isBuffer()) {
