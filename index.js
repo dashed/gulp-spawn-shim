10c65a9f564113d53ae5ccdf4f6bf8db3bb8cb65
Alberto Leal <mail4alberto@gmail.com>
2014-01-13 11:15:49 -0500

    Resolve race condition for child.stdout

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ _queue = require('async-queue-stream'),
 gutil = require('gulp-util'),
 through = require('through');
 
-
 function gulp_spawn_shim(_opts) {
 
     var opts = _opts || {};
@@ -34,6 +33,7 @@ function gulp_spawn_shim(_opts) {
     opts.template.extname = _opts.template.extname || "<%= extname %>";
     opts.template.filename = _opts.template.extname || "<%= filename %>";
 
+    // micro-template args
     var config_args = function(file) {
 
         // template the args
@@ -54,10 +54,10 @@ function gulp_spawn_shim(_opts) {
      * - stderr
      * - exit (exit code)
      *
+     * TODO: merge them?
      */
     var super_bus = new events.EventEmitter();
 
-    //ENOENT
 
     var _write = function(file, cb) {
 
@@ -75,38 +75,42 @@ function gulp_spawn_shim(_opts) {
          * stdout-data
          * stdout-end
          * tmp-file-create
-         * tmp-file-clean
          */
         var bus = new events.EventEmitter();
 
         // properly invoke callback once
-        bus.once('publish', function(err, file) {
+        bus.once('publish', function(err, _file) {
             if(err) {
                 bus.removeAllListeners();
             }
-            console.log('publish:' + path.basename(file.path));
 
-            return cb(err, file);
+            if(_file) {
+                console.log('publish:' + path.basename(file.path));
+            } else {
+                console.log('did not publish:' + path.basename(file.path));
+            }
+
+
+            return cb(err, _file);
         });
 
-
+        // micro-template args
         config_args(file);
 
-        // handle stream errors
+        // handle stream error appropriately
         err_catcher.once('error', function(err) {
 
             // Broken pipe
             if (err.code == "EPIPE" || err.code == "ENOENT") {
                 return bus.emit('publish');
             }
-            console.log('CAUGHT ERROR: ' + err);
-            console.log('FILE: ' + file.path);
+            // console.log('CAUGHT ERROR: ' + err);
+            // console.log('FILE: ' + file.path);
             bus.emit('publish', err);
         });
 
 
-        var child = void 0;
-        child = spawn(opts.cmd, opts.args, opts.options);
+        var child = spawn(opts.cmd, opts.args, opts.options);
 
         // catch a lot of errors
         err_catcher.add(child);
@@ -128,7 +132,6 @@ function gulp_spawn_shim(_opts) {
 
         // capture spawn exit code
         child.on('close', function(num) {
-
             return super_bus.emit('exit', num);
         });
 
@@ -142,12 +145,7 @@ function gulp_spawn_shim(_opts) {
              * stdout-data
              * stdout-end
              * tmp-file-create
-             * tmp-file-clean
              */
-
-            bus.on('tmp-file-clean', function() {
-                tmp.setGracefulCleanup();
-            });
 
             // Attempt to write to stdin
             file.contents
@@ -160,19 +158,23 @@ function gulp_spawn_shim(_opts) {
                     // console.log('ended!');
                 });
 
+            tmp.setGracefulCleanup();
 
             // 1. Create tmp file
             tmp.file(function _tempFileCreated(err, tmp_path, fd) {
                 if (err) return bus.emit('publish', err);
 
                 if(child.stdout.readable === false) {
-                    return bus.emit('publish'), bus.emit('tmp-file-clean');
+                    tmp.setGracefulCleanup();
+                    return bus.emit('publish');
                 }
+
+
 
                 var tmp_file = fs.createWriteStream(tmp_path);
 
-                // 2. Attempt to write to tmp file
-                child.stdout.pipe(tmp_file);
+
+                // child.stdout.pipe(tmp_file);
 
                 // tmp_file.on('end', function() {
                 //     console.log('ended')
@@ -180,136 +182,34 @@ function gulp_spawn_shim(_opts) {
 
                 var written_tmp = false;
                 child.stdout
-                    .once('readable', function() {
-                        // bus.emit('tmp-file-create');
-                    })
-                    .once('data', function() {
+                    // 2. Attempt to write to tmp file
+                    .on('data', function(data) {
                         written_tmp = true;
-                        console.log("DID WRITE:" + path.basename(file.path));
+                        tmp_file.write(data);
+                        // REMOVE
+                        // console.log("DID WRITE:" + path.basename(file.path));
                     })
                     .on('end', function() {
 
                         if(written_tmp === false) {
-
                             return bus.emit('publish');
                         }
 
                         file.contents = fs.createReadStream(tmp_path);
+
+                        // REMOVE
+                        file.contents.on('end', function() {
+                            console.log('closed tmp file:' + path.basename(file.path));
+                        });
 
                         return bus.emit('publish', void 0, file);
                     });
 
             });
 
-
-
             return;
-
-            // var readable = function(count, callback) {
-            //     var self = this;
-            // };
-
-            // var writable = es.readable(readable);
-
-            bus.on('stdout-end', function() {
-                // writable.emit('end');
-            });
-
-            bus.on('tmp-file-create', function() {
-
-                tmp.file(function _tempFileCreated(err, tmp_path, fd) {
-                    if (err) return bus.emit('publish', err);
-
-                    if(child.stdout.readable === false) {
-                        return bus.emit('publish'), bus.emit('tmp-file-clean');
-                    }
-
-                    // write into tmp file
-                    var tmp_file = fs.createWriteStream(tmp_path);
-                    err_catcher.add(tmp_file);
-
-                    child.stdout
-                        .pipe(tmp_file);
-
-                    child.stdout
-                        .on('end', function() {
-
-                            var read_stream = fs.createReadStream(tmp_path);
-
-                            read_stream.on('end', function() {
-                                tmp.setGracefulCleanup();
-                                console.log('clean up: ' + path.basename(file.path));
-                            });
-
-                            file.contents = read_stream;
-
-                            console.log('published: '+ path.basename(file.path));
-
-                            return bus.emit('publish', void 0, file);
-
-                        });
-
-                });
-            });
-
-
-
-            return;
-
-
-            tmp.file(function _tempFileCreated(err, tmp_path, fd) {
-                if (err) err_catcher.emit('error', err);
-
-                // console.log(Object.keys());
-                if(child.stdout.readable === false) {
-                    tmp.setGracefulCleanup();
-
-                    // err_catcher.emit('error', new Error('not readable'));
-                    return cb();
-                }
-
-                // read from tmp_file and write into it
-                var tmp_file = fs.createWriteStream(tmp_path);
-
-                child.stdout
-                    .pipe(tmp_file);
-
-                child.on('close', function(num) {
-                    console.log(_stderr);
-
-                    console.log('exit: ' + num);
-                });
-
-                child.stdout
-                    .once('end', function(){
-
-                        var read_stream = fs.createReadStream(tmp_path);
-
-                        read_stream.on('end', function() {
-                            tmp.setGracefulCleanup();
-                            console.log('clean up: ' + path.basename(file.path));
-                        });
-
-                        file.contents = read_stream;
-
-                        console.log('published: '+ file.path);
-                        return cb(null, file);
-                    });
-
-            });
-
-
-
-
-
-            return;
-
-            // return publish(file);
-            // return this.emit('error', new gutil.PluginError('gulp-pandoc', 'Streaming not supported.'));
 
         } else if (file.isBuffer()) {
-
-            console.log('why are u here?')
 
             var output = new Buffer(0);
 
@@ -319,12 +219,12 @@ function gulp_spawn_shim(_opts) {
 
             child.stdout.once("end", function () {
                     file.contents = output;
-                    publish(file);
+                    return bus.emit('publish', void 0, file);
             });
 
             child.stdin.write(file.contents);
             child.stdin.end();
-
+            return;
         }
 
     };
